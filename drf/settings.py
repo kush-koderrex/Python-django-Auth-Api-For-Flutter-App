5f308637c3b361557fb0dc13ed1d1a49831a9648
kush (Koderrex) <56927996+kush-koderrex@users.noreply.github.com>
2021-10-06 02:20:57 +0530

    Update settings.py

--- drf/settings.py
+++ drf/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)ojcy7%y^b!q=p6n8clu*vj(d_uyma*gp*!_)=mt9)_2-@&#=4'
+SECRET_KEY = 'enter your key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
